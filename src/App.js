26d7bf3801f58eff102ee6a24dedc08fe7c39144
tinykite <dakota@tinykitelab.com>
2020-02-23 10:27:53 -0600

    Add reusable components for interstitial and coffee methods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Hero from './components/Hero';
+import Introduction from './components/Introduction';
+import Interstitial from './components/Interstitial';
+import Aeropress from './components/Aeropress';
+import KalitaWave from './components/KalitaWave';
+import FrenchPress from './components/FrenchPress';
 
 function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <Hero />
+      <Introduction />
+      <Interstitial title="Brew Methods" icon="Tools" />
+      <Aeropress
+        title="Aeropress"
+        fontStack="Barbour"
+        background="#19224f"
+      />
+      <KalitaWave />
+      <FrenchPress />
+      <Interstitial title="Technique" icon="Technique" />
     </>
   );
 }
---
+++ src/components/TechniqueItems.jsx
@@ -0,0 +1,29 @@
+export const TechniqueSections = [
+  {
+    title: 'Strength',
+    text: [
+      'In high school I thought that strong coffee was the kind of thing that punched you in the face and made you speak like a Hemingway novel.',
+      "And to credit my heart-on-sleeve teenage self, it does heavily influence our perception of flavor. But the term doesn't accurately qualify how aggressively macho (or even how long) something was brewed.",
+      'Strength is predominantly determined, before brewing, by the ratio of coffee to hot water that you use: and for adequately strong coffee, coffee educators typically recommend staying between 1:15 and 1:17.',
+      'For 8oz of coffee (using a 1:15 ratio), that means a dose of approximately ~15 grams of beans.',
+    ],
+    backgroundColor: '#000000',
+    foregroundColor: '#FFFFFF',
+    layout: 'odd',
+  },
+  {
+    title: 'Grind',
+    text: [
+      'Many guides share the same seemingly immutable tips about how to grind coffee:',
+      "Using a whirly boi (or blade grinder) on your beans is garbage. The Baratza Encore is lovely. You'll make more flavorful coffee if you avoid pre-ground beans — as well as coffee labeled without an obvious roast date.",
+      "It's all great. It also feels like your nutritionist telling you that sugar is inflammatory.",
+      'How do you improve upon bad coffee? According to Counter Culture, by making frequent and tiny adjustments to your grind based off of taste.',
+      'If your mouth feels dry with an ashy aftertaste, you’re likely grinding too fine.',
+      'If the coffee tastes weak, sour, or a little papery, you may be grinding too coarse.',
+    ],
+    backgroundColor: '#F6EFDF',
+  },
+  {
+    title: 'Time',
+  },
+];
