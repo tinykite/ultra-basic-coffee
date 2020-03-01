3b8e895f981d3cf017fa1535a3440a6b9fd012d8
tinykite <dakota@tinykitelab.com>
2020-03-01 15:13:56 -0600

    Remove unecessary styled components import

--- src/components/Strength.js
+++ src/components/Strength.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import styled from 'styled-components';
 import {
   ScrollContainer,
   TechniqueMain,
--- src/components/Time.js
+++ src/components/Time.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import styled from 'styled-components';
 import { useOnScreen } from '../hooks/useOnScreen';
 import {
   ScrollContainer,
