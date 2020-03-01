e15e7691678e9c05c7c6d0dfe7c9d857e1dfc246
tinykite <dakota@tinykitelab.com>
2020-03-01 16:04:42 -0600

    Adjust visual hierarchy, add coffee text as bg image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const HeroContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: black;
+  padding: 50px 0;
 
   @media (min-width: 450px) {
     padding: 8vh 0;
@@ -26,7 +26,7 @@ const HeroContainer = styled.div`
 const HeroTitle = styled.h1`
   font-family: 'Knockout 69 A', 'Knockout 69 B';
   font-style: normal;
-  font-size: 60px;
+  font-size: 70px;
   font-weight: 400;
   color: #ffffff;
   text-align: center;
@@ -37,8 +37,8 @@ const HeroTitle = styled.h1`
   line-height: 1;
   text-align: center;
 
-  @media (min-width: 700px) {
-    font-size: 85px;
+  @media (min-width: 600px) {
+    font-size: 100px;
   }
 
   @media (min-width: 1000px) {
@@ -46,24 +46,17 @@ const HeroTitle = styled.h1`
   }
 `;
 
-const HeroIllustrationContainer = styled.div`
-  display: block;
-  width: 280px;
-  z-index: 100;
-  background: green;
-`;
-
 const HeroIllustration = styled.svg`
   height: auto;
-  margin-top: 30px;
-  width: 370px;
+  width: 240px;
+  margin-top: 20px;
   color: white;
   position: relative;
   fill: none;
-  background: blue;
 
-  @media (min-width: 1000px) {
-    margin-top: 40px;
+  @media (min-width: 720px) {
+    width: 370px;
+    margin-top: 20px;
   }
 `;
 
@@ -98,43 +91,41 @@ const IntroDescription = styled.p`
 const Hero = () => {
   return (
     <>
-      <HeroContainer>
+      <HeroContainer className="c-coffeeText">
         <HeroTitle>
           Ultra Basic<span className="u-visuallyHidden">Coffee</span>
         </HeroTitle>
-        <HeroIllustrationContainer>
-          <HeroIllustration viewBox="0 0 370 220">
-            <rect width="370" height="220" stroke-width="12" />
-            <rect
-              x="6"
-              y="6"
-              width="310"
-              height="210"
-              stroke-width="12"
-              stroke="currentColor"
-              fill="none"
-            />
-            <rect
-              width="48"
-              height="150"
-              x="316"
-              y="35"
-              stroke="currentColor"
-              stroke-width="12"
-              fill="none"
-            />
-            <motion.rect
-              initial={{ translateY: '100%' }}
-              animate={{ translateY: 0 }}
-              transition={{ duration: 8 }}
-              x="0"
-              y="0"
-              width="316"
-              height="220"
-              fill="currentColor"
-            />
-          </HeroIllustration>
-        </HeroIllustrationContainer>
+        <HeroIllustration viewBox="0 0 370 220">
+          <rect width="370" height="220" stroke-width="12" />
+          <rect
+            x="6"
+            y="6"
+            width="310"
+            height="210"
+            stroke-width="12"
+            stroke="currentColor"
+            fill="none"
+          />
+          <rect
+            width="48"
+            height="150"
+            x="316"
+            y="35"
+            stroke="currentColor"
+            stroke-width="12"
+            fill="none"
+          />
+          <motion.rect
+            initial={{ translateY: '100%' }}
+            animate={{ translateY: 0 }}
+            transition={{ duration: 8 }}
+            x="0"
+            y="0"
+            width="316"
+            height="220"
+            fill="currentColor"
+          />
+        </HeroIllustration>
       </HeroContainer>
       <Intro>
         <IntroDescription>
