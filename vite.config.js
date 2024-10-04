03424aba33f1886c63ea8abc648ad23808a380e6
tinykite <dakota@tinykitelab.com>
2024-10-04 09:08:11 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   css: {
     postcss: './postcss.config.js',
   },
